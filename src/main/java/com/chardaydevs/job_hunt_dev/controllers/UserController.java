8c9f181ae656e8ed2278b675fcbc95a72927db12
Charday Neal <charday.neal@gmail.com>
2025-01-22 15:40:43 -0800

    Add PUT and DELETE path routes for User Model

--- src/main/java/com/chardaydevs/job_hunt_dev/controllers/UserController.java
+++ src/main/java/com/chardaydevs/job_hunt_dev/controllers/UserController.java
@@ -19,32 +19,66 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    @PostMapping("")
+    public User createUser(@RequestBody User user) {
+        if (user.getName() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error Creating User: must include name");
+        } else if (user.getEmail() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error Creating User: must include email");
+        }
+
+        return this.userRepository.save(user);
+    }
+
+
     @GetMapping("")
     public Iterable<User> getAllUsers() {
         return this.userRepository.findAll();
     }
 
+
     @GetMapping("/{id}")
-    public Optional<User> getUserById(@PathVariable("id") String id) {
+    public User getUserById(@PathVariable("id") String id) {
+        return validateUser(id);
+    }
+
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User u) {
+        User foundUser = validateUser(id);
+
+//        validate request Body
+        if (u.getName() != null) {
+            foundUser.setName(u.getName());
+        }
+        if (u.getEmail() != null) {
+            foundUser.setEmail(u.getEmail());
+        }
+        return this.userRepository.save(foundUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") String id) {
+        User foundUser = validateUser(id);
+        this.userRepository.delete(foundUser);
+
+        return "User successfully deleted";
+    }
+
+
+    public User validateUser(String id) {
         try {
             Integer userId = Integer.parseInt(id);
 
-            Optional<User> user = this.userRepository.findById(userId);
-            if (!user.isPresent()) {
-                String errorString = String.format("Error Fetching User: %s does not exist", id);
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cannot retrieve User: does not exist");
+            Optional<User> foundUser = this.userRepository.findById(userId);
+            if (foundUser.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error Fetching User: User does not exist");
             }
-            return user;
+
+            return foundUser.get();
         } catch (NumberFormatException error) {
             String errorString = String.format("Error Fetching User: %s is an invalid id", id);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errorString);
         }
     }
-
-    @PostMapping("")
-    public User createUser(@RequestBody User user) {
-        User newUser = this.userRepository.save(user);
-        return newUser;
-    }
-
 }
